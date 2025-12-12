6f3851c48d5b2ae125c26a2711f7b6e26cdc6dcc
GoGradually <chkun3109@gmail.com>
2025-12-12 17:21:08 +0900

    feat: gRPC 애플리케이션 시작 클래스 추가

---
+++ src/main/java/me/example/helloworld/GrpcHelloApplication.java
@@ -0,0 +1,11 @@
+package me.example.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcHelloApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcHelloApplication.class, args);
+    }
+}
